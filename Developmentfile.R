3c80bd8151b4a7681f358381f59d12ef9b3f53e8
Mauricio Vela <mauricioandresvela@gmail.com>
2016-03-23 20:06:44 -0400

    Better start creating separate R files

--- Developmentfile.R
+++ Developmentfile.R
@@ -10,16 +10,17 @@ library(roxygen2)
 
 
 #Create method trapezoid that needs to have x vector , y vector , a and b limit values and the output from the integration
-setClass(Class="Trapezoid", slots = c(x = "numeric", y = "numeric", a = "numeric", b = "numeric", n= "numeric" ),
-         prototype = prototype( x = numeric(), y = numeric(), a = numeric(), b = numeric(), n = numeric()), 
+setClass(Class="Trapezoid", slots = c(x = "numeric", y = "numeric", a = "numeric", b = "numeric"),
+         prototype = prototype( x = numeric(), y = numeric(), a = numeric(), b = numeric()), 
         validity=function(object){
           # Validity to check there are no missing arguments
           if(is.null(object@x) | is.null(object@y) | is.null(object@a) | is.null(object@b)){
              stop("All x, y, a and b need to be specified")
            }
-           # make sure n 
-           if(object@n %% 1 != 0 | object@n < 0){
-             stop("N must be a positive integer")
-           }
-         }
+           # make sure a and b are different and be most be greater to a
+          if(index.a >= index.b){
+            stop("a most be different from b, and b most be greater to a")
+          }
+        }
 )
+
